211d9bc3142d1502c77e498c8a61c09fa89a4f8c
Daniel Valencia <charaprogram19@gmail.com>
2022-07-18 21:13:45 -0500

    este es la actividad 1 js back-end

---
+++ app.js
@@ -0,0 +1,6 @@
+let name = 'chara'
+let lastName = 'sepulveda'
+let yearofBirth = 1996
+let cityofbirth = 'medellin'
+
+console.log( `hola, mi nombre es ${name} ${lastName}, vivo en ${cityofbirth} y tengo ${2022 - yearofBirth} años`)
